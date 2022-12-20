4632e8e505e2a3d7f22e4ba47e85ea43abe3c68b
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2022-12-19 22:39:18 -0300

    Create Gradient Colors

--- App_Login_SignUP_Onboarding/App_Login_SignUP_Onboarding/Controller/LoginController.swift
+++ App_Login_SignUP_Onboarding/App_Login_SignUP_Onboarding/Controller/LoginController.swift
@@ -5,4 +5,21 @@
 //  Created by Kaique Lopes on 19/12/22.
 //
 
-import Foundation
+import UIKit
+
+    // MARK: - Criando uma tela LoginController
+class LoginController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Criando Cor Gradiente
+    func configureUI() {
+        let gradientColor = CAGradientLayer()
+        gradientColor.colors = [UIColor.systemPurple.cgColor, UIColor.blue.cgColor]
+        gradientColor.locations = [0, 1]
+        view.layer.addSublayer(gradientColor)
+        gradientColor.frame = view.frame
+    }
+}
