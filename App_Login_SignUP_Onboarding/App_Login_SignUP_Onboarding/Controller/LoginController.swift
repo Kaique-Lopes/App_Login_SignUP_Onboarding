c1dc56ca26d5b6de0d6125bec1d2ff73eba96306
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2022-12-19 23:00:45 -0300

    Create Components and Setup Constraints

--- App_Login_SignUP_Onboarding/App_Login_SignUP_Onboarding/Controller/LoginController.swift
+++ App_Login_SignUP_Onboarding/App_Login_SignUP_Onboarding/Controller/LoginController.swift
@@ -9,17 +9,40 @@ import UIKit
 
     // MARK: - Criando uma tela LoginController
 class LoginController: UIViewController {
+        // MARK: - Components
+    lazy var iconImage: UIImageView = {
+        let icon = UIImageView(image: UIImage(named: "firebase-logo"))
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        return icon
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        addSubViews()
+        setupConstraints()
     }
     
     // MARK: - Criando Cor Gradiente
     func configureUI() {
+    // MARK: - Hidden NavigationBar
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
         let gradientColor = CAGradientLayer()
         gradientColor.colors = [UIColor.systemPurple.cgColor, UIColor.blue.cgColor]
         gradientColor.locations = [0, 1]
         view.layer.addSublayer(gradientColor)
         gradientColor.frame = view.frame
+    }
+    
+    func addSubViews() {
+        view.addSubview(iconImage)
+    }
+    
+    func setupConstraints() {
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 120, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
     }
 }
