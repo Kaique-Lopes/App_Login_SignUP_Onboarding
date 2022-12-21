6db21a7e0ea361e213416ef85c3c0f9d570ec62f
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2022-12-20 23:42:58 -0300

    Create news componentes
    Create componente passwordTextField, emailTextField and setup constraints

--- App_Login_SignUP_Onboarding/App_Login_SignUP_Onboarding/Controller/LoginController.swift
+++ App_Login_SignUP_Onboarding/App_Login_SignUP_Onboarding/Controller/LoginController.swift
@@ -12,8 +12,41 @@ class LoginController: UIViewController {
         // MARK: - Components
     lazy var iconImage: UIImageView = {
         let icon = UIImageView(image: UIImage(named: "firebase-logo"))
-        icon.translatesAutoresizingMaskIntoConstraints = false
         return icon
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        
+        let emailTF = UITextField()
+        emailTF.leftView = spacer
+        emailTF.leftViewMode = .always
+        emailTF.keyboardType = .emailAddress
+        emailTF.borderStyle = .none
+        emailTF.textColor = .white
+        emailTF.keyboardAppearance = .dark
+        emailTF.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        emailTF.setHeight(height: 50)
+        emailTF.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return emailTF
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        
+        let passTF = UITextField()
+        passTF.leftView = spacer
+        passTF.leftViewMode = .always
+        passTF.isSecureTextEntry = true
+        passTF.borderStyle = .none
+        passTF.textColor = .white
+        passTF.keyboardAppearance = .dark
+        passTF.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        passTF.setHeight(height: 50)
+        passTF.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return passTF
     }()
     
     override func viewDidLoad() {
@@ -44,5 +77,16 @@ class LoginController: UIViewController {
         iconImage.centerX(inView: view)
         iconImage.setDimensions(height: 120, width: 120)
         iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stackView = UIStackView(arrangedSubviews: [ emailTextField, passwordTextField ])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        view.addSubview(stackView)
+        stackView.anchor(top: iconImage.bottomAnchor,
+                         left: view.leftAnchor,
+                         right: view.rightAnchor,
+                         paddingTop: 32,
+                         paddingLeft: 32,
+                         paddingRight: 32)
     }
 }
