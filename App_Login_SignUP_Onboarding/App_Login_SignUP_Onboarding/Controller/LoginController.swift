8df7ec3def17796dfb04addda6e5a80137ed088d
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2022-12-21 22:17:46 -0300

    refactor viewcontroller
    create customTextField

--- App_Login_SignUP_Onboarding/App_Login_SignUP_Onboarding/Controller/LoginController.swift
+++ App_Login_SignUP_Onboarding/App_Login_SignUP_Onboarding/Controller/LoginController.swift
@@ -7,48 +7,23 @@
 
 import UIKit
 
-    // MARK: - Criando uma tela LoginController
 class LoginController: UIViewController {
-        // MARK: - Components
+    
+    // MARK: - Components
     lazy var iconImage: UIImageView = {
         let icon = UIImageView(image: UIImage(named: "firebase-logo"))
         return icon
     }()
     
-    lazy var emailTextField: UITextField = {
-        let spacer = UIView()
-        spacer.setDimensions(height: 50, width: 12)
-        
-        let emailTF = UITextField()
-        emailTF.leftView = spacer
-        emailTF.leftViewMode = .always
-        emailTF.keyboardType = .emailAddress
-        emailTF.borderStyle = .none
-        emailTF.textColor = .white
-        emailTF.keyboardAppearance = .dark
-        emailTF.backgroundColor = UIColor(white: 1, alpha: 0.1)
-        emailTF.setHeight(height: 50)
-        emailTF.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
-        return emailTF
-    }()
+    lazy var emailTextField = CustomTextField(placeholder: "E-mail")
     
-    lazy var passwordTextField: UITextField = {
-        let spacer = UIView()
-        spacer.setDimensions(height: 50, width: 12)
-        
-        let passTF = UITextField()
-        passTF.leftView = spacer
-        passTF.leftViewMode = .always
+    lazy var passwordTextField: CustomTextField = {
+        let passTF = CustomTextField(placeholder: "Password")
         passTF.isSecureTextEntry = true
-        passTF.borderStyle = .none
-        passTF.textColor = .white
-        passTF.keyboardAppearance = .dark
-        passTF.backgroundColor = UIColor(white: 1, alpha: 0.1)
-        passTF.setHeight(height: 50)
-        passTF.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
         return passTF
     }()
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -58,7 +33,6 @@ class LoginController: UIViewController {
     
     // MARK: - Criando Cor Gradiente
     func configureUI() {
-    // MARK: - Hidden NavigationBar
         navigationController?.navigationBar.isHidden = true
         navigationController?.navigationBar.barStyle = .black
         
@@ -69,10 +43,12 @@ class LoginController: UIViewController {
         gradientColor.frame = view.frame
     }
     
+    // MARK: - Adicionando elementos na view
     func addSubViews() {
         view.addSubview(iconImage)
     }
     
+    // MARK: - Criando Constraints
     func setupConstraints() {
         iconImage.centerX(inView: view)
         iconImage.setDimensions(height: 120, width: 120)
